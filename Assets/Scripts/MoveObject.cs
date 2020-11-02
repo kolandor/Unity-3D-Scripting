04677c5316baf7d8bfc609c10f304ddabd652863
tomacamlar <tomacamlar@protonmail.com>
2020-11-02 20:32:01 +0200

    Init

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("I am still here!");
+    }
+}
