3218b1fef76ff88dc4f821b2fb2f4fff8f93c21d
tomacamlar <tomacamlar@protonmail.com>
2020-11-03 01:21:35 +0200

    Add MovedObject script

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MoveObject : MonoBehaviour
 {
+    private GameObject MovedObject;
+
+    public Transform TargetPosition;
+
+    public float Speed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("I am alive!");
+        MovedObject = base.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("I am still here!");
+        MoveObjectToTarget();
+    }
+
+    void MoveObjectToTarget()
+    {
+        Vector3 positionFrom = MovedObject.transform.position;
+        Vector3 positionTo = TargetPosition.position;
+        float currentSpeed = Time.deltaTime * Speed;
+        MovedObject.transform.position = Vector3.MoveTowards(
+            positionFrom, positionTo, currentSpeed);
     }
 }
