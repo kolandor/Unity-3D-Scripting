eb15ff0ddde44c352caf68afa01f85654e12279f
tomacamlar <tomacamlar@protonmail.com>
2020-11-03 21:51:36 +0200

    Add EnemyObjectDestroyer

---
+++ Assets/Scripts/EnemyObjectDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyObjectDestroyer : MonoBehaviour
+{
+    public GameObject TargetObject;
+
+    public bool TargetDestroyObjectByGoalTrget = true;
+
+    public bool SelfDestroyObjectByGoalTrget = false;
+
+    public float DestroyDistanceFromTarget = 0.5f;
+
+    public float DestroyTimeByGoalTrget = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
